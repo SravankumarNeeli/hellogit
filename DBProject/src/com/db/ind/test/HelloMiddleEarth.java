ed9764feb42f43d89750f3c437e3f0d162be8cd5
sravan <neelisravan.aws@gmail.com>
2018-02-24 11:32:15 +0530

    Comitted HelloMiddleEarth of DBProject

---
+++ DBProject/src/com/db/ind/test/HelloMiddleEarth.java
@@ -0,0 +1,14 @@
+package com.db.ind.test;
+
+public class HelloMiddleEarth {
+
+	public static void main(String[] args) {
+		
+		String[] names= {"Sravan","Haritha","Heman","Baby"};
+		for(int i=0;i<names.length;i++) {
+			System.out.format("Hello \n",names[i]);
+		}
+
+	}
+
+}
